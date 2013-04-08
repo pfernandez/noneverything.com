52e068c7468fa72e6d5e688a8f775e8fbce6781a
Paul Fernandez <paul4nandez@gmail.com>
2013-04-07 17:26:10 -0700

    minor cleanup

--- app.js
+++ app.js
@@ -1,32 +1,29 @@
-
 /**
  * Module dependencies.
  */
 
+// Load module dependencies.
 var express = require('express')
 var http    = require('http')
 var path    = require('path');
 
+// Innstantiate the app.
 var app = express();
 
-// all environments
+// Set up the environment.
 app.set('port', process.env.PORT || 3000);
 app.use(express.favicon());
 app.use(express.logger('dev'));
 app.use(express.bodyParser());
-//app.use(express.methodOverride());
-//  app.use(express.cookieParser('your secret here'));
-//  app.use(express.session());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'public')));  // use static files in 'public/'
+app.use(express.directory(__dirname + '/public'));  // show directory listings 
 
-// show directory listings
-app.use(express.directory(__dirname + '/public'));
-
-// development only
+// Handle errors depending on mode: 'development' or 'production'.
 if ('development' == app.get('env')) {
-  app.use(express.errorHandler());
+    app.use(express.errorHandler());
 }
 
-http.createServer(app).listen(app.get('port'), function(){
-  console.log('Express server listening on port ' + app.get('port'));
+http.createServer(app).listen(app.get('port'), function() {
+    console.log('Express server listening on port ' + app.get('port')
+        + ' in ' + app.get('env') + ' mode.');
 });
